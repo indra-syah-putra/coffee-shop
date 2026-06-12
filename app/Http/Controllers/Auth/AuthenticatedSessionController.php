6b0e079f755fc1af02216c4b83812e11c05253c2
25272928 <114021578+25272928@users.noreply.github.com>
2026-06-12 19:37:20 +0700

    fix: redirect admin logout to admin login portal

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -51,12 +51,14 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
+        $isAdmin = $request->user()?->is_admin;
+
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return $isAdmin ? redirect()->route('admin.login') : redirect('/');
     }
 }
